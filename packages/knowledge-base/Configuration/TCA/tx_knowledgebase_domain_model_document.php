6a8d27cf0a0d90d20aa09ea73c36a5315f57e035
Marei Büsing <marei.buesing@hees.de>
2026-04-13 11:58:07 +0100

    add rte configuration, stylings and headings

--- packages/knowledge-base/Configuration/TCA/tx_knowledgebase_domain_model_document.php
+++ packages/knowledge-base/Configuration/TCA/tx_knowledgebase_domain_model_document.php
@@ -37,6 +37,7 @@ return [
             'config' => [
                 'type' => 'text',
                 'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
                 'rows' => 15,
             ],
         ],
--- packages/knowledge-base/ext_localconf.php
+++ packages/knowledge-base/ext_localconf.php
@@ -1,3 +1,7 @@
 <?php
 
 defined('TYPO3') || die('Access denied.');
+
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] =
+    "EXT:knowledge-base/Configuration/Extensions/RteCkEditor/default.yaml";
