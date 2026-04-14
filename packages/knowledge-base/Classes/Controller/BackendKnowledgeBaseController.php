f01506a527f7066cc0b87b8d9f10aa5e62ada96a
Marei Büsing <marei.buesing@hees.de>
2026-04-14 15:21:49 +0100

    update structure

--- packages/knowledge-base/Classes/Controller/BackendKnowledgeBaseController.php
+++ packages/knowledge-base/Classes/Controller/BackendKnowledgeBaseController.php
@@ -31,6 +31,7 @@ class BackendKnowledgeBaseController extends ActionController
     {
         $this->moduleTemplate = $this->moduleTemplateFactory->create($this->request);
         $this->pageRenderer->addCssFile('EXT:knowledge-base/Resources/Public/Css/Backend.css');
+        $this->pageRenderer->addJsFile('EXT:knowledge-base/Resources/Public/JavaScript/Backend.js');
     }
 
     public function indexAction(int $openDocumentId = 0): ResponseInterface
