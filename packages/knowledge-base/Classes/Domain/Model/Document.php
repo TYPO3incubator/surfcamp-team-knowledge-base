26b76713dd29b9cd18ee8297f7ff35c2ef412154
Matthias Böhm <matthias.boehm@reelworx.at>
2026-04-16 09:55:36 +0100

    [TASK] Add breadcrumbs to document json

--- packages/knowledge-base/Classes/Domain/Model/Document.php
+++ packages/knowledge-base/Classes/Domain/Model/Document.php
@@ -129,6 +129,7 @@ class Document extends AbstractEntity implements \JsonSerializable
             'parent' => $this->getParent()?->getUid(),
             'status' => $this->getStatus()?->getUid(),
             'user' => $this->getUser()?->getUid(),
+            'breadcrumbs' => $this->getBreadcrumbs(),
         ];
     }
 }
