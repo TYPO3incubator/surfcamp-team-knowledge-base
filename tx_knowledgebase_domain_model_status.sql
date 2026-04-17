cea7ec17c7c2598e95c112756851e282e4a14c56
MoritzEckert <moritz.eckert@netlogix.de>
2026-04-17 16:32:11 +0100

    feat: add status sql

---
+++ tx_knowledgebase_domain_model_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO db.tx_knowledgebase_domain_model_status (uid, pid, tstamp, crdate, deleted, hidden, document, title, ordering) VALUES (10, 1, 1776500000, 1776500000, 0, 0, 60, 'To Do', '0');
+INSERT INTO db.tx_knowledgebase_domain_model_status (uid, pid, tstamp, crdate, deleted, hidden, document, title, ordering) VALUES (11, 1, 1776500000, 1776500000, 0, 0, 60, 'In Progress', '1');
+INSERT INTO db.tx_knowledgebase_domain_model_status (uid, pid, tstamp, crdate, deleted, hidden, document, title, ordering) VALUES (12, 1, 1776500000, 1776500000, 0, 0, 60, 'Done', '2');
